45a4420ea913385be285036e3b7c41595823f527
Markus Getto <MarkusGetto@gmail.com>
2016-04-05 19:51:15 +0200

    initial commit with project template

---
+++ Buffer/src/TestBuffer.cpp
@@ -0,0 +1,14 @@
+#include "../includes/Buffer.h"
+
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+
+//	Buffer*  buffer;
+
+//	buffer = new Buffer();
+
+	std::cout << "Hello!" << std::endl;
+
+}
